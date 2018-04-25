e31bf523337aa4e9b580cf2c3d0ddedc88063d77
Zaryab Farooq <zfarooq@ucdavis.edu>
2018-04-25 14:40:40 -0700

    Add files via upload

---
+++ energy_img.m
@@ -0,0 +1,14 @@
+% Kiranpreet Kaur
+% 04/20/2018
+% energyImg
+
+function energyImg = energy_img(im)
+    im = (uint8(double(imread('inputSeamCarvingPrague.jpg'))));
+    im = rgb2gray(im);  %converting to grayscale
+    [dx, dy] = imgradient(im);  % get the dx and dy
+    energyImg = double(sqrt(dx.^2 + dy.^2));  % energy image from the formulae
+    imagesc(energyImg)
+end 
+
+
+
