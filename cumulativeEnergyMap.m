384a14d13891374f2aae39c84b7316ea3f7cf2cf
zaryabfarooq <zfarooq@ucdavis.edu>
2018-04-25 14:45:54 -0700

    energy_img function working

--- cumulativeEnergyMap.m
+++
@@ -1,7 +0,0 @@
-% Kiranpreet Kaur
-% 04/20/2018
-% cumulativeEnergyMap
-
-function cumulativeEnergyMap = cumulative_min_energy_map(energyImg,seamDirection)
-    cumulativeEnergyMap = energyImg(i,j) + min(cumulativeEnergyMap(i-1,j-1),cumulativeEnergyMap(i-1,j), cumulativeEnergyMap(i-1,j+1)); 
-end
