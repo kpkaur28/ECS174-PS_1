99a606e2af74f82b920603ccef4674eccb0aab75
Kiranpreet Kaur <35642755+kpkaur28@users.noreply.github.com>
2018-04-25 20:00:00 -0700

    Update script.m

--- script.m
+++ script.m
@@ -3,8 +3,18 @@
 % ECS 174
 % Script
 
+% Part 1
 energyImage = energy_img(imread('inputSeamCarvingPrague.jpg')); 
+Figure(1);
 imagesc(energyImage);
 energyImage = energy_img(imread('inputSeamCarvingMall.jpg)'));
+Figure(2);
 imagesc(energyImage);
-cumulative_min_energy_map(energImg, seamDirection)
+seamDirection = 'VERTICAL';
+cumulative_min_energy_map(energImg, seamDirection);
+Figure(3);
+imagesc(cumulative_min_energy_map); 
+seamDirection = 'HORZONTAL';
+cumulative_min_energy_map(energyImg, seamDirection);
+Figure(4);
+imagesc(cumulative_min_energy_map);
