37382ba43c5e08156d965a5e0f837c6dddc12a39
Kiranpreet Kaur <35642755+kpkaur28@users.noreply.github.com>
2018-04-25 19:56:33 -0700

    Add files via upload

---
+++ script.m
@@ -0,0 +1,10 @@
+% Kiranpreet Kaur
+% Zaryab Farooq
+% ECS 174
+% Script
+
+energyImage = energy_img(imread('inputSeamCarvingPrague.jpg')); 
+imagesc(energyImage);
+energyImage = energy_img(imread('inputSeamCarvingMall.jpg)'));
+imagesc(energyImage);
+cumulative_min_energy_map(energImg, seamDirection)
